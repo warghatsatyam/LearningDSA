1310236cc2406af56fa2c5b7a57af14deb07a709
satyam warghat <warghatsatyam33@gmail.com>
2024-03-03 17:52:33 +0530

    Optimized version of pair sum to zero

--- Dictionary/pair_sum_to_zero.py
+++ Dictionary/pair_sum_to_zero.py
@@ -1,16 +1,33 @@
+# def get_pair_sum_to_zero_count(arr):
+#     count = 0
+#     n = len(arr)
+#     for i in range(n):
+#         ele1 = arr[i]
+#         for j in range(i+1,n):
+#             ele2 = arr[j]
+#             if ele1+ele2 == 0:
+#                 count+=1
+#     return count
+
 def get_pair_sum_to_zero_count(arr):
     count = 0
-    n = len(arr)
-    for i in range(n):
-        ele1 = arr[i]
-        for j in range(i+1,n):
-            ele2 = arr[j]
-            if ele1+ele2 == 0:
-                count+=1
+    d = {} 
+    for x in  arr:
+        if x in d:
+            d[x] = d.get(x)+1
+        else:
+            d[x] = 1
+    for x in d :
+        if x !=0 and -(x) in d:
+            count += d[x]*d[-x]
+            d[x]=0
+            d[-x]=0
+        if x == 0 and d[x]!=1:
+            val = d[x]
+            count+=((val-1)*val)//2
     return count
 
-
 if __name__ == '__main__':
-    arr = [2,0,1,-2,4]
+    arr = [0,0,0,0]
     count = get_pair_sum_to_zero_count(arr)
     print(count)
