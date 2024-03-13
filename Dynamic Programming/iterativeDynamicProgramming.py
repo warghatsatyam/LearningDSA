9ba669531413dce40075e54c420f9e64caf03887
satyam warghat <warghatsatyam33@gmail.com>
2024-03-13 21:40:40 +0530

    Iterative Fibonacci Series

---
+++ Dynamic Programming/iterativeDynamicProgramming.py
@@ -0,0 +1,17 @@
+
+def Ifib(n,dp):
+    dp[0]=0
+    dp[1]=1
+    i=2
+    while i<=n:
+        dp[i]=dp[i-1]+dp[i-2]
+        i+=1
+    return dp[n]
+
+
+
+if __name__ == '__main__':
+    n=int(input())
+    dp=[-1 for i in range(n+1)]
+    ans = Ifib(n,dp)
+    print(ans)
