b2cad4888baf7c48ecd385bb0e60eced88bb4426
satyam warghat <warghatsatyam33@gmail.com>
2024-03-17 10:46:07 +0530

    Renamed filename and functionname following best practices

--- Dynamic Programming/minimum_step_to_one.py
+++ Dynamic Programming/minimum_step_to_one.py
@@ -1,19 +1,19 @@
 
-def minimumStepToOneCode(n):
+def minimum_step_to_one_code(n):
     if n==1:
         return 0
-    ans1 = minimumStepToOneCode(n-1)
+    ans1 = minimum_step_to_one_code(n-1)
     if n%2==0:
-        ans2 = minimumStepToOneCode(n//2)
+        ans2 = minimum_step_to_one_code(n//2)
     else:
         ans2=float('inf')
     if n%3==0:
-        ans3 = minimumStepToOneCode(n//3)
+        ans3 = minimum_step_to_one_code(n//3)
     else:
         ans3=float('inf')
     return 1 + min(ans1,ans2,ans3)
 
 if __name__ == '__main__':
     n = int(input("Enter value of n: "))
-    minimumNumber = minimumStepToOneCode(n)
+    minimumNumber = minimum_step_to_one_code(n)
     print(minimumNumber)
