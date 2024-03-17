61838aabb619eefa1c66b68605df12759fa5e013
satyam warghat <warghatsatyam33@gmail.com>
2024-03-17 10:41:56 +0530

    Changed the code logic and followed best practice for writing function name and filename

--- Dynamic Programming/minimumStepToOneMemoization.py
+++ Dynamic Programming/minimumStepToOneMemoization.py
@@ -1,17 +1,17 @@
-def minimumStepToOneCode(n, dp):
+def minimum_step_to_one_using_memoization(n, dp):
     if n==0:
         return -1
     if n == 1:
         dp[n] = 0
         return 0
     if dp[n] == -1:
-        ans1 = minimumStepToOneCode(n - 1, dp)
+        ans1 = minimum_step_to_one_using_memoization(n - 1, dp)
         dp[n] = ans1
     else:
         ans1 = dp[n]
     if n % 2 == 0:
         if dp[n // 2] == -1:
-            ans2 = minimumStepToOneCode(n // 2, dp)
+            ans2 = minimum_step_to_one_using_memoization(n // 2, dp)
             dp[n // 2] = ans2
         else:
             ans2 = dp[n // 2]
@@ -19,7 +19,7 @@ def minimumStepToOneCode(n, dp):
         ans2 = float('inf')
     if n % 3 == 0:
         if dp[n // 3] == -1:
-            ans3 = minimumStepToOneCode(n // 3, dp)
+            ans3 = minimum_step_to_one_using_memoization(n // 3, dp)
             dp[n // 3] = ans3
         else:
             ans3 = dp[n // 3]
@@ -30,5 +30,5 @@ def minimumStepToOneCode(n, dp):
 if __name__ == '__main__':
     n = int(input("Enter value of n: "))
     dp = [-1 for _ in range(n + 1)]
-    minimumNumber = minimumStepToOneCode(n, dp)
+    minimumNumber = minimum_step_to_one_using_memoization(n, dp)
     print(minimumNumber)
