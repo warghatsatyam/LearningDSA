e8e15390f59700fdff03307f1b65df996972c8c3
satyam warghat <warghatsatyam33@gmail.com>
2024-03-27 22:27:22 +0530

    Working on Knapscak Memoization

--- Dynamic Programming/zero_one_knapscak_using_memoization.py
+++ Dynamic Programming/zero_one_knapscak_using_memoization.py
@@ -1,10 +1,13 @@
-def knapsack_using_memoization():
+def knapsack_using_memoization(W,v,wt):
     pass 
 
 
 
 if __name__ == '__main__':
-    ans = knapsack_using_memoization()
+    W = 5
+    v = [100,200,500]
+    wt = [1,2,3]
+    ans = knapsack_using_memoization(W,v,wt)
     print(ans)
 
     
