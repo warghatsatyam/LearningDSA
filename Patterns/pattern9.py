21e0e4ac803291b549d68c3f00f2553b0104b3dc
satyam warghat <warghatsatyam33@gmail.com>
2024-05-01 16:46:35 +0530

    Pattern 9

---
+++ Patterns/pattern9.py
@@ -0,0 +1,15 @@
+def pattern_nine(n):
+    for i in range(1,n+1):
+        for j in range(1,i):
+            print(" ",end=" ")
+
+        for k in range(0,n-i+1):
+            print("*",end=" ")
+        
+        for l in range(0,n-i):
+            print("*",end=" ")
+        print()
+
+if __name__ == '__main__':
+    n = int(input("Enter the value: "))
+    pattern_nine(n)
