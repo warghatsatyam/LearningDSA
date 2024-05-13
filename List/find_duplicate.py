3d5778bf7ade2027e4778945bbc566a1802e4ebd
satyam warghat <warghatsatyam33@gmail.com>
2024-05-13 22:27:06 +0530

    Find Duplicate

---
+++ List/find_duplicate.py
@@ -0,0 +1,21 @@
+def get_duplicatet(arr):
+    hashmap = {}
+    for x in arr:
+        a = hashmap.get(x,0)
+        if a==0:
+            hashmap[x]=1
+        else:
+            hashmap[x]+=1
+    for x in hashmap.keys():
+        if hashmap[x] == 2:
+            return x
+    return -1
+
+
+
+
+
+if __name__ == '__main__':
+    arr = [0,7,2,6,4,7,1,3,6]
+    unique_ele = get_duplicatet(arr)
+    print(unique_ele)
