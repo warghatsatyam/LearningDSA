47a4fe0a3aafdb720f4b5610321bd505e7f390b7
Satyam Warghat <warghatsatyam33@gmail.com>
2024-05-16 10:55:52 +0530

    Update count_pair_sum.py

--- List/count_pair_sum.py
+++ List/count_pair_sum.py
@@ -1,17 +1,20 @@
 
-def get_count_pair_sum(arr,sum):
-    count = 0 
+def pairSum(arr,n,sum) :
+    count = 0
     hashmap = {}
     for x in arr:
         if hashmap.get(x,0)==0:
             hashmap[x]=1
         else:
             hashmap[x]+=1
+    
     for x in arr:
-        diff = sum - x 
-        if diff in hashmap and hashmap[diff]>0:
-            count+=1
-            hashmap[diff]-=1
+        if hashmap[x]!=0:
+            compl = sum-x
+            if compl in hashmap and hashmap[compl]!=0:
+                count+=hashmap[x]*hashmap[compl]
+                hashmap[x]=0
+                hashmap[compl]=0
     return count
 
 
