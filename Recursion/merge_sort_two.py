3bb9275a6020b2f1dc26bc40a07164afb1526236
satyam warghat <warghatsatyam33@gmail.com>
2024-05-29 09:00:10 +0530

    Merge Sort 2

---
+++ Recursion/merge_sort_two.py
@@ -0,0 +1,11 @@
+def merge():
+    pass 
+
+def merge_sort_code_two(arr):
+    pass 
+
+if __name__ == '__main__':
+    arr1 = [2,3,4]
+    arr2 = [0,1,5]
+    arr = [2,4,3,0,5,1]
+    output = merge(arr,arr1,arr2)
