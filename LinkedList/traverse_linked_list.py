5e27bd9828ca0517a0ff70ed58be4c40b3297a76
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-02 04:13:52 +0000

    Traverse Linked List

---
+++ LinkedList/traverse_linked_list.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,data):
+        self.data = data 
+        self.next = None 
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+head = node1
+
+def traverse_linked_list(head):
+    while head is not None:
+        print(head.data, end=" ")
+        head = head.next
+
+
+traverse_linked_list(head)
