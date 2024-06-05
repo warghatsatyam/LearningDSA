94eed6c4b4fb85a05c52efa28546ee82118828e5
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-05 07:05:37 +0000

    Modified Prime Algo using Sqrt and ceil

--- MATH/prims_algo.py
+++ MATH/prims_algo.py
@@ -1,9 +1,9 @@
-
+import math 
 def print_prime(n):
     prime_num = []
     for i in range(2,n+1):
         is_prime = True 
-        for j in range(2,9):
+        for j in range(2,math.ceil(math.sqrt(n))):
             if i%j == 0 and i!=j:
                 is_prime = False 
                 break 
@@ -11,8 +11,7 @@ def print_prime(n):
             prime_num.append(i)
     print(prime_num)
 
-import math
 
-n = 50 
+n = 122
 print_prime(n)
         
