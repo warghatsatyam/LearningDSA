4a84f9f12d6bf6bc87d98e33ca533153aa58e1a3
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-06 03:19:39 +0000

    Index of node in a Linked List

---
+++ LinkedList/get_index_of_node.py
@@ -0,0 +1,27 @@
+from optimized_input_taking_of_linkedlist import optimized_input_taking_ll
+def get_index_of_node_code(head,data):
+    if head is None:
+        return -1 
+    is_present = False
+    pos = 0
+    while head is not None:
+        if head.data == data:
+            is_present = True
+            break
+        else:
+            pos+=1
+            head = head.next 
+    if is_present:
+        return pos 
+    return -1 
+
+
+if __name__  == '__main__':
+    arr = [int(x) for x in input().split()]
+    head = optimized_input_taking_ll(arr)
+    data = int(input())
+    node_position = get_index_of_node_code(head, data)
+    if node_position!=-1:
+        print(f"The node  {data} is present at {node_position}")
+    else:
+        print(f"The node {data} is not present")
