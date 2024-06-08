99b6ae7ed25e199d5b172432fe2de7f70bea6788
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-08 06:32:38 +0000

    Reverser  a Linked List Using Iterative Approach

---
+++ LinkedList/reverse_linked_list_using_iterativley.py
@@ -0,0 +1,19 @@
+from optimized_input_taking_of_linkedlist import optimized_input_taking_ll,traversal_ll
+
+def reverse_linked_list_using_iteratively_code(head):
+    previous = None 
+    current = head 
+    while current:
+        next_node = current.next 
+        current.next = previous
+        previous = current
+        current = next_node
+    head = previous
+    return head 
+
+
+if __name__ == "__main__":
+    arr = [int(x) for x in input().split()]
+    head = optimized_input_taking_ll(arr)
+    reverse_head = reverse_linked_list_using_iteratively_code(head)
+    traversal_ll(reverse_head)
