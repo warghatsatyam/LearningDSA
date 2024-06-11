517e870db273fc7120e1829fb74d7a3820938699
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-11 03:35:25 +0000

    Modified Stack using Array added Gaurdian Pattern

--- Stack/intro_to_stack.py
+++ Stack/intro_to_stack.py
@@ -22,18 +22,18 @@ class Stack:
         ele = self.__arr[self.size -1 ]
         return ele 
     
-s = Stack()
-s.push(10)
-s.push(11)
-s.push(12)
-s.push(13)
-s.push(14)
-print(s.size_of_stack())
-print(s.top())
-print(s.isEmpty())
-print(s.pop())
-print(s.pop())
-print(s.pop())
-print(s.size_of_stack())
-
+if __name__ == '__main__':
+    s = Stack()
+    s.push(10)
+    s.push(11)
+    s.push(12)
+    s.push(13)
+    s.push(14)
+    print(s.size_of_stack())
+    print(s.top())
+    print(s.isEmpty())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.size_of_stack())
 
