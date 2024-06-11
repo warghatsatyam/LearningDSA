d73ef423dbef78a0747760e8441cd89b2d8d697d
Satyam Warghat <warghatsatyam33@gmail.com>
2024-06-11 03:17:06 +0000

    Stack Created Using Array

--- Stack/intro_to_stack.py
+++ Stack/intro_to_stack.py
@@ -3,5 +3,37 @@ class Stack:
         self.__arr = []
         self.size = len(self.__arr)
 
+    def push(self,data):
+        self.__arr.append(data)
+        self.size+=1
+
+    def pop(self):
+        ele = self.__arr.pop()
+        self.size-=1
+        return ele 
+    
+    def size_of_stack(self):
+        return self.size
+    
+    def isEmpty(self):
+        return self.size == 0
+    
+    def top(self):
+        ele = self.__arr[self.size -1 ]
+        return ele 
     
 s = Stack()
+s.push(10)
+s.push(11)
+s.push(12)
+s.push(13)
+s.push(14)
+print(s.size_of_stack())
+print(s.top())
+print(s.isEmpty())
+print(s.pop())
+print(s.pop())
+print(s.pop())
+print(s.size_of_stack())
+
+
